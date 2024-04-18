1483153fc7e5b07c4969c462845c68702ad79806
OhJeMIN <jasonoh22@naver.com>
2024-04-18 17:38:32 +0900

    feat: 체크 시 상태 변경 추가

--- src/main/java/com/ll/flow_assignment/FileExtension/controller/ExtensionController.java
+++ src/main/java/com/ll/flow_assignment/FileExtension/controller/ExtensionController.java
@@ -2,9 +2,14 @@ package com.ll.flow_assignment.FileExtension.controller;
 
 import com.ll.flow_assignment.FileExtension.service.ExtensionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,5 +21,13 @@ public class ExtensionController {
         model.addAttribute("fixedExtensions", extensionService.findNotCustom());
         model.addAttribute("customExtensions", extensionService.findCustom());
         return "extensions";
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> updateExtensionStatus(@RequestBody Map<String, Object> payload) {
+        String extension = (String) payload.get("extension");
+        boolean checked = (Boolean) payload.get("checked");
+        extensionService.update(extension, checked);
+        return ResponseEntity.ok().build();
     }
 }
