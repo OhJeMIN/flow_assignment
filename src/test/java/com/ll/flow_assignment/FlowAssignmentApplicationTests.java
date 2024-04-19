408fe2c885dca029d6c99c3cc8f72507678eb50f
OhJeMIN <jasonoh22@naver.com>
2024-04-19 17:31:57 +0900

    feat: 배포 세팅

--- src/test/java/com/ll/flow_assignment/FlowAssignmentApplicationTests.java
+++ src/test/java/com/ll/flow_assignment/FlowAssignmentApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.flow_assignment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FlowAssignmentApplicationTests {
 
 	@Test
